6276918e31e8aa392afc63ed32996c246206a348
desplega <desplega@desplega.com>
2024-02-19 12:34:21 +0000

    Getting Single Record and API Resources

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return Category::all();
+        return CategoryResource::collection(Category::all());
+    }
+
+    public function show(Category $category) 
+    {
+        return new CategoryResource($category);
     }
 }
