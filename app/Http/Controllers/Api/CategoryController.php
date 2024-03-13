6ec84b2858b66dd2af1ce8abf6ab8fcfb06d726c
desplega <desplega@desplega.com>
2024-03-13 15:03:29 +0000

    Finishing CRUD: Update, Delete and Resource Controller

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreCategoryRequest;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Http\Resources\CategoryResource;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -25,5 +26,19 @@ class CategoryController extends Controller
         $category = Category::create($request->all());
 
         return new CategoryResource($category);
+    }
+
+    public function update(Category $category, StoreCategoryRequest $request)
+    {
+        $category->update($request->all());
+
+        return new CategoryResource($category);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
